b4a2dceea416f09679e7acc8f88a30eebc7f6325
Hanson Emmanuel <hansonemma398@gmail.com>
2026-05-15 22:52:38 +0100

    Tested if the tailwindcss setup worked

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <section id="center">
-        <h1 className="text-3xl text-red-500">Bif</h1>
+      <section>
+        <h1 className="text-6xl text-red-500">DevSnap</h1>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
